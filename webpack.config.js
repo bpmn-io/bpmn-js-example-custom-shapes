266bfb94275ac35a9f1b69965c6b9c44f1bc6ee0
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-11-08 11:47:53 +0100

    chore: build using webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+const path = require('path');
+
+const basePath = '.';
+
+const absoluteBasePath = path.resolve(path.join(__dirname, basePath));
+
+module.exports = {
+  mode: 'development',
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.bpmn$/,
+        type: 'asset/source'
+      }
+    ]
+  },
+  resolve: {
+    mainFields: [
+      'browser',
+      'module',
+      'main'
+    ],
+    modules: [
+      'node_modules',
+      absoluteBasePath
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'app/index.html', to: '.' },
+        { from: 'node_modules/bpmn-js/dist/assets', to: 'vendor/bpmn-js/assets' }
+      ]
+    })
+  ]
+};
